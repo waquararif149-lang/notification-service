d47e8a62f2e789aaf9e34a4116905383061d09f6
Arif Waquar <waquararif149@gmail.com>
2026-07-13 13:21:18 +0530

    chore:added push,sms,dlq on bullboard

--- src/config/bullboard.js
+++ src/config/bullboard.js
@@ -3,13 +3,19 @@ import { BullMQAdapter } from "@bull-board/api/bullMQAdapter";
 import { ExpressAdapter } from "@bull-board/express";
 
 import { emailQueue } from "../queue/email.queue.js";
+import { pushQueue } from "../queue/push.queue.js";
+import { smsQueue } from "../queue/sms.queue.js";
+import { emailDLQ } from "../queue/DLQ.queue.js";
 
 const serverAdapter=new ExpressAdapter();
 serverAdapter.setBasePath("/admin/queues");
 
 createBullBoard({
     queues: [
-        new BullMQAdapter(emailQueue)
+        new BullMQAdapter(emailQueue),
+        new BullMQAdapter(pushQueue),
+        new BullMQAdapter(smsQueue),
+        new BullMQAdapter(emailDLQ)
     ],
     serverAdapter
 });
